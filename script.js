fa98abefdca82df574bd9600037c38b6581c32f3
Michael McIsaac <mmcisaac@upei.ca>
2024-02-25 10:45:10 -0400

    undo bugs introduced.

--- script.js
+++ script.js
@@ -454,7 +454,6 @@ function swapTiles(targetCell, draggedTile) {
         // If dragged from a cell, swap the tiles
         targetCell.appendChild(draggedTile);
         draggedTileParent.appendChild(targetCell.firstChild);
-        
     } else {
         // If dragged from the tiles container, move the target tile to the container and the dragged tile to the cell
         document.getElementById('tiles').appendChild(targetCell.firstChild);
