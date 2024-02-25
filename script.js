38a7b5d267e2e45f6af4bd98704e67662c87ed9e
Michael McIsaac <mmcisaac@upei.ca>
2024-02-25 10:40:15 -0400

    fixing touch drag

--- script.js
+++ script.js
@@ -225,16 +225,10 @@ function handleTouchStart(e) {
     e.preventDefault();
     const target = e.target;
 
-    // Check if the target has a first child and remove classes if so
-    if (target.firstChild) {
-        target.firstChild.classList.remove('clue-correct', 'clue-partial', 'clue-incorrect');
-    }
-
     // Set the element being dragged
     target.classList.add('dragging');
     activeTile = target;
 }
-
 
 function handleTouchMove(e) {
     e.preventDefault();
